f4988ce83e0ba7f0dc05a9e7019928fbffa8225d
Jaelad <mkserkan@gmail.com>
2022-09-16 12:01:18 +0300

    applyHPChange from AlwaysHP module

--- module/actor-manager.js
+++ module/actor-manager.js
@@ -6,12 +6,48 @@ export default class ActorManager {
 		this._ = MKS
 	}
 
-	hpChange(tokenOrActor, change) {
-		const actor = tokenOrActor?.actor ?? tokenOrActor
-		const {max, value} = actor.data.data.attributes.hp
-		if ((change > 0 && max === value) || (change < 0 && value === 0) || change === 0)
-			return
-		return actor.update({_id: actor.id, "data.attributes.hp.value": Math.min(max, Math.max(0, value + change))})
+	// from AllwaysHP module
+	async applyHPChange(token, change, multiplier = 1) {
+		const actor = token.actor
+		const hp = actor.system.attributes.hp
+		const resourcename = "system.attributes.hp"
+		const updates = {}
+
+		const target = change.target
+		const value = Math.floor(parseInt(change.value) * multiplier) * -1 // implement as damage
+
+		// Deduct damage from temp HP first
+		if (hp.hasOwnProperty("tempmax") && target === "max") {
+			updates[resourcename + ".tempmax"] = (hp.tempmax ?? 0) - value
+		}
+		else {
+			let dt = 0, tmpMax = 0
+			if (hp.hasOwnProperty("temp")) {
+				const tmp = parseInt(hp.temp) || 0
+				dt = (value > 0 || target === 'temp') && target !== 'regular' && target !== 'max' ? Math.min(tmp, value) : 0
+				// Remaining goes to health
+				tmpMax = parseInt(hp.tempmax) || 0
+				updates[resourcename + ".temp"] = tmp - dt
+			}
+
+			// Update the Actor
+			if (target !== 'temp' && target !== 'max' && dt >= 0) {
+				let change = (value - dt)
+				updates[resourcename + ".value"] = Math.clamped(hp.value - change, 0, hp.max + tmpMax)
+
+				let display = change + dt
+				canvas.interface.createScrollingText(token.center, (-display).signedString(), {
+					anchor: CONST.TEXT_ANCHOR_POINTS.CENTER,
+					direction: display > 0 ? CONST.TEXT_ANCHOR_POINTS.BOTTOM : CONST.TEXT_ANCHOR_POINTS.TOP,
+					distance: token.h,
+					fontSize: 28,
+					stroke: 0x000000,
+					strokeThickness: 4,
+					jitter: 0.25
+				}).then()
+			}
+		}
+		return await actor.update(updates)
 	}
 
 }
